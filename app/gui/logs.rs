cc7a5f68cf2051fb0939a60ce06228ce21747700
Ash Manning <ash@layertwolabs.com>
2024-03-05 22:24:59 +0800

    improve logs capture, bump version number

--- app/gui/logs.rs
+++ app/gui/logs.rs
@@ -11,9 +11,8 @@ impl Logs {
 
     pub fn show(&self, ui: &mut egui::Ui) {
         egui::CentralPanel::default().show(ui.ctx(), |ui| {
-            let logs_read = self.0.logs.read();
-            let mut text: &str =
-                unsafe { std::str::from_utf8_unchecked(&logs_read) };
+            let text_read = self.0.as_str();
+            let mut text: &str = &text_read;
             TextEdit::multiline(&mut text)
                 .font(TextStyle::Monospace)
                 .desired_width(f32::INFINITY)
