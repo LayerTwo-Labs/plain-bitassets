a634198eb0bb634f6ae71505623b27fd19d23a89
Ash Manning <ash@layertwolabs.com>
2024-03-05 22:59:46 +0800

    improve logger gui, bump version number

--- app/gui/logs.rs
+++ app/gui/logs.rs
@@ -1,4 +1,4 @@
-use eframe::egui::{self, TextEdit, TextStyle, Widget as _};
+use eframe::egui::{self, ScrollArea, TextEdit, TextStyle, Widget as _};
 
 use crate::logs::LogsCapture;
 
@@ -10,13 +10,13 @@ impl Logs {
     }
 
     pub fn show(&self, ui: &mut egui::Ui) {
-        egui::CentralPanel::default().show(ui.ctx(), |ui| {
-            let text_read = self.0.as_str();
-            let mut text: &str = &text_read;
+        let text_read = self.0.as_str();
+        let mut text: &str = &text_read;
+        ScrollArea::vertical().stick_to_bottom(true).show(ui, |ui| {
             TextEdit::multiline(&mut text)
                 .font(TextStyle::Monospace)
                 .desired_width(f32::INFINITY)
-                .ui(ui)
+                .ui(ui);
         });
     }
 }
