06222583edbd204b56e49e35f9fa8559d4714e34
Ash Manning <ash@layertwolabs.com>
2025-02-03 20:55:02 +0800

    Add print_stderr and print_stdout lints

--- cli/main.rs
+++ cli/main.rs
@@ -5,6 +5,9 @@ use plain_bitassets_app_cli_lib::Cli;
 async fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
     let res = cli.run().await?;
-    println!("{res}");
+    #[allow(clippy::print_stdout)]
+    {
+        println!("{res}");
+    }
     Ok(())
 }
