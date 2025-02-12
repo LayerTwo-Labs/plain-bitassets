LayerTwo-Labs/plain-bitnames

Use version in heartbeat messages, add RPCs to get blocks/bmm inclusions, bump version for release

app/rpc_server.rs
@@ -24,26 +24,34 @@ pub struct RpcServerImpl {
     app: App,
 }
 
-fn custom_err(err_msg: impl Into<String>) -> ErrorObject<'static> {
+fn custom_err_msg(err_msg: impl Into<String>) -> ErrorObject<'static> {
     ErrorObject::owned(-1, err_msg.into(), Option::<()>::None)
+}
+
+fn custom_err<Error>(error: Error) -> ErrorObject<'static>
+where
+    anyhow::Error: From<Error>,
+{
+    let error = anyhow::Error::from(error);
+    custom_err_msg(format!("{error:#}"))
 }
 
 fn convert_app_err(err: app::Error) -> ErrorObject<'static> {
     let err = anyhow::anyhow!(err);
     tracing::error!("{err:#}");
-    custom_err(err.to_string())
+    custom_err(err)
 }
 
 fn convert_node_err(err: node::Error) -> ErrorObject<'static> {
     let err = anyhow::anyhow!(err);
     tracing::error!("{err:#}");
-    custom_err(err.to_string())
+    custom_err(err)
 }
 
 fn convert_wallet_err(err: wallet::Error) -> ErrorObject<'static> {
     let err = anyhow::anyhow!(err);
     tracing::error!("{err:#}");
-    custom_err(err.to_string())
+    custom_err(err)
 }
 
 #[async_trait]
@@ -114,6 +122,16 @@ impl RpcServer for RpcServerImpl {
         Ok(block)
     }
 
+    async fn get_bmm_inclusions(
+        &self,
+        block_hash: plain_bitnames::types::BlockHash,
+    ) -> RpcResult<Vec<bitcoin::BlockHash>> {
+        self.app
+            .node
+            .get_bmm_inclusions(block_hash)
+            .map_err(custom_err)
+    }
+
     async fn get_new_address(&self) -> RpcResult<Address> {
         self.app
             .wallet
@@ -181,7 +199,7 @@ impl RpcServer for RpcServerImpl {
         let fee_sats = filled_tx
             .transaction
             .fee()
-            .map_err(|err| custom_err(format!("{err:#}")))?
+            .map_err(custom_err)?
             .unwrap()
             .to_sat();
         let res = TxInfo {
@@ -325,8 +343,7 @@ impl RpcServer for RpcServerImpl {
         let memo = match memo {
             None => None,
             Some(memo) => {
-                let hex = hex::decode(memo)
-                    .map_err(|err| custom_err(err.to_string()))?;
+                let hex = hex::decode(memo).map_err(custom_err)?;
                 Some(hex)
             }
         };

cli/lib.rs
@@ -30,6 +30,10 @@ pub enum Command {
     GenerateMnemonic,
     /// Get block data
     GetBlock { block_hash: BlockHash },
+    /// Get mainchain blocks that commit to a specified block hash
+    GetBmmInclusions {
+        block_hash: plain_bitnames::types::BlockHash,
+    },
     /// Get a new address
     GetNewAddress,
     /// Get a new encryption pubkey
@@ -184,6 +188,11 @@ impl Cli {
             Command::GetBlockcount => {
                 let blockcount = rpc_client.getblockcount().await?;
                 format!("{blockcount}")
+            }
+            Command::GetBmmInclusions { block_hash } => {
+                let bmm_inclusions =
+                    rpc_client.get_bmm_inclusions(block_hash).await?;
+                serde_json::to_string_pretty(&bmm_inclusions)?
             }
             Command::GetNewAddress => {
                 let address = rpc_client.get_new_address().await?;

rpc-api/lib.rs
@@ -93,6 +93,16 @@ pub trait Rpc {
     #[method(name = "get_block")]
     async fn get_block(&self, block_hash: BlockHash) -> RpcResult<Block>;
 
+    /// Get mainchain blocks that commit to a specified block hash
+    #[open_api_method(output_schema(
+        PartialSchema = "bitnames_schema::BitcoinBlockHash"
+    ))]
+    #[method(name = "get_bmm_inclusions")]
+    async fn get_bmm_inclusions(
+        &self,
+        block_hash: plain_bitnames::types::BlockHash,
+    ) -> RpcResult<Vec<bitcoin::BlockHash>>;
+
     /// Get a new address
     #[method(name = "get_new_address")]
     async fn get_new_address(&self) -> RpcResult<Address>;

LayerTwo-Labs/plain-bitassets

app/rpc_server.rs
@@ -27,26 +27,34 @@ pub struct RpcServerImpl {
     app: App,
 }
 
-fn custom_err(err_msg: impl Into<String>) -> ErrorObject<'static> {
+fn custom_err_msg(err_msg: impl Into<String>) -> ErrorObject<'static> {
     ErrorObject::owned(-1, err_msg.into(), Option::<()>::None)
+}
+
+fn custom_err<Error>(error: Error) -> ErrorObject<'static>
+where
+    anyhow::Error: From<Error>,
+{
+    let error = anyhow::Error::from(error);
+    custom_err_msg(format!("{error:#}"))
 }
 
 fn convert_app_err(err: app::Error) -> ErrorObject<'static> {
     let err = anyhow::anyhow!(err);
     tracing::error!("{err:#}");
-    custom_err(err.to_string())
+    custom_err(err)
 }
 
 fn convert_node_err(err: node::Error) -> ErrorObject<'static> {
     let err = anyhow::anyhow!(err);
     tracing::error!("{err:#}");
-    custom_err(err.to_string())
+    custom_err(err)
 }
 
 fn convert_wallet_err(err: wallet::Error) -> ErrorObject<'static> {
     let err = anyhow::anyhow!(err);
     tracing::error!("{err:#}");
-    custom_err(err.to_string())
+    custom_err(err)
 }
 
 #[async_trait]
@@ -360,6 +368,16 @@ impl RpcServer for RpcServerImpl {
         Ok(block)
     }
 
+    async fn get_bmm_inclusions(
+        &self,
+        block_hash: plain_bitassets::types::BlockHash,
+    ) -> RpcResult<Vec<bitcoin::BlockHash>> {
+        self.app
+            .node
+            .get_bmm_inclusions(block_hash)
+            .map_err(custom_err)
+    }
+
     async fn get_new_address(&self) -> RpcResult<Address> {
         self.app
             .wallet
@@ -423,7 +441,7 @@ impl RpcServer for RpcServerImpl {
         let fee_sats = filled_tx
             .transaction
             .bitcoin_fee()
-            .map_err(|err| custom_err(format!("{err:#}")))?
+            .map_err(custom_err)?
             .unwrap()
             .to_sat();
         let res = TxInfo {
@@ -588,8 +606,7 @@ impl RpcServer for RpcServerImpl {
         let memo = match memo {
             None => None,
             Some(memo) => {
-                let hex = hex::decode(memo)
-                    .map_err(|err| custom_err(err.to_string()))?;
+                let hex = hex::decode(memo).map_err(custom_err)?;
                 Some(hex)
             }
         };

cli/lib.rs
@@ -82,6 +82,10 @@ pub enum Command {
     GetBlock { block_hash: BlockHash },
     /// Get the current block count
     GetBlockcount,
+    /// Get mainchain blocks that commit to a specified block hash
+    GetBmmInclusions {
+        block_hash: plain_bitassets::types::BlockHash,
+    },
     /// Get a new address
     GetNewAddress,
     /// Get a new encryption pubkey
@@ -264,6 +268,11 @@ impl Cli {
             Command::GetBlockcount => {
                 let blockcount = rpc_client.getblockcount().await?;
                 format!("{blockcount}")
+            }
+            Command::GetBmmInclusions { block_hash } => {
+                let bmm_inclusions =
+                    rpc_client.get_bmm_inclusions(block_hash).await?;
+                serde_json::to_string_pretty(&bmm_inclusions)?
             }
             Command::GetNewAddress => {
                 let address = rpc_client.get_new_address().await?;

rpc-api/lib.rs
@@ -37,9 +37,9 @@ pub struct TxInfo {
     bitassets_schema::BitcoinTransaction, bitassets_schema::BitcoinOutPoint,
     Address, AssetId, Authorization, BitAssetData, BitAssetDataUpdates,
     BitAssetId, BitcoinOutputContent, BlockHash, Body, DutchAuctionId,
-    DutchAuctionParams, EncryptionPubKey, Header, MerkleRoot, OutPoint, Output,
-    OutputContent, Transaction, TxData, Txid, TxIn, WithdrawalOutputContent,
-    VerifyingKey,
+    DutchAuctionParams, EncryptionPubKey, FilledOutputContent, Header,
+    MerkleRoot, OutPoint, Output, OutputContent, Transaction, TxData, Txid,
+    TxIn, WithdrawalOutputContent, VerifyingKey,
 ])]
 #[rpc(client, server)]
 pub trait Rpc {
@@ -181,6 +181,16 @@ pub trait Rpc {
     #[open_api_method(output_schema(ToSchema))]
     #[method(name = "get_block")]
     async fn get_block(&self, block_hash: BlockHash) -> RpcResult<Block>;
+
+    /// Get mainchain blocks that commit to a specified block hash
+    #[open_api_method(output_schema(
+        PartialSchema = "bitassets_schema::BitcoinBlockHash"
+    ))]
+    #[method(name = "get_bmm_inclusions")]
+    async fn get_bmm_inclusions(
+        &self,
+        block_hash: plain_bitassets::types::BlockHash,
+    ) -> RpcResult<Vec<bitcoin::BlockHash>>;
 
     /// Get a new address
     #[method(name = "get_new_address")]