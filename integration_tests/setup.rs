LayerTwo-Labs/plain-bitnames

add integration test

integration_tests/setup.rs
@@ -1,0 +1,287 @@
+use std::{
+    net::{Ipv4Addr, SocketAddrV4},
+    path::PathBuf,
+    time::Duration,
+};
+
+use bip300301_enforcer_integration_tests::{
+    setup::{PostSetup as EnforcerPostSetup, Sidechain},
+    util::AbortOnDrop,
+};
+use bip300301_enforcer_lib::types::SidechainNumber;
+use futures::{channel::mpsc, future, TryFutureExt as _};
+use plain_bitnames::types::{FilledOutput, FilledOutputContent, PointedOutput};
+use plain_bitnames_app_rpc_api::RpcClient as _;
+use reserve_port::ReservedPort;
+use thiserror::Error;
+use tokio::time::sleep;
+
+use crate::util::BitNamesApp;
+
+#[derive(Debug)]
+pub struct ReservedPorts {
+    pub net: ReservedPort,
+    pub rpc: ReservedPort,
+    pub zmq: ReservedPort,
+}
+
+impl ReservedPorts {
+    pub fn new() -> Result<Self, reserve_port::Error> {
+        Ok(Self {
+            net: ReservedPort::random()?,
+            rpc: ReservedPort::random()?,
+            zmq: ReservedPort::random()?,
+        })
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum BmmError {
+    #[error(transparent)]
+    Mine(#[from] bip300301_enforcer_integration_tests::mine::MineError),
+    #[error(transparent)]
+    RpcClient(#[from] jsonrpsee::core::ClientError),
+}
+
+#[derive(Debug, Error)]
+pub enum SetupError {
+    #[error("Failed to create BitNames dir")]
+    CreateBitNamesDir(#[source] std::io::Error),
+    #[error(transparent)]
+    ReservePort(#[from] reserve_port::Error),
+    #[error(transparent)]
+    RpcClient(#[from] jsonrpsee::core::ClientError),
+}
+
+#[derive(Debug, Error)]
+pub enum ConfirmDepositError {
+    #[error(transparent)]
+    Bmm(#[from] BmmError),
+    #[error("Deposit not found with txid: `{txid}`")]
+    DepositNotFound { txid: bitcoin::Txid },
+    #[error(transparent)]
+    RpcClient(#[from] jsonrpsee::core::ClientError),
+}
+
+#[derive(Debug, Error)]
+pub enum CreateWithdrawalError {
+    #[error(transparent)]
+    Bmm(#[from] BmmError),
+    #[error("Pending withdrawal bundle not found")]
+    PendingWithdrawalBundleNotFound,
+    #[error(transparent)]
+    RpcClient(#[from] jsonrpsee::core::ClientError),
+}
+
+#[derive(Debug)]
+pub struct PostSetup {
+    // MUST occur before temp dirs and reserved ports in order to ensure that processes are dropped
+    // before reserved ports are freed and temp dirs are cleared
+    pub _bitnames_app_task: AbortOnDrop<()>,
+    /// RPC client for bitnames_app
+    pub rpc_client: jsonrpsee::http_client::HttpClient,
+    /// Address for receiving deposits
+    pub deposit_address: plain_bitnames::types::Address,
+    // MUST occur after tasks in order to ensure that tasks are dropped
+    // before reserved ports are freed
+    pub reserved_ports: ReservedPorts,
+}
+
+impl PostSetup {
+    /// BMM a block
+    pub async fn bmm_single(
+        &self,
+        post_setup: &mut EnforcerPostSetup,
+    ) -> Result<(), BmmError> {
+        use bip300301_enforcer_integration_tests::mine::mine;
+        let ((), ()) = future::try_join(
+            self.rpc_client.mine(None).map_err(BmmError::from),
+            async {
+                sleep(Duration::from_secs(1)).await;
+                mine::<Self>(post_setup, 1, Some(true))
+                    .await
+                    .map_err(BmmError::from)
+            },
+        )
+        .await?;
+        Ok(())
+    }
+
+    /// BMM blocks
+    pub async fn bmm(
+        &self,
+        post_setup: &mut EnforcerPostSetup,
+        blocks: u32,
+    ) -> Result<(), BmmError> {
+        for i in 0..blocks {
+            tracing::debug!("BMM block {}/{blocks}", i + 1);
+            let () = self.bmm_single(post_setup).await?;
+        }
+        Ok(())
+    }
+
+    pub fn net_port(&self) -> u16 {
+        self.reserved_ports.net.port()
+    }
+
+    pub fn net_addr(&self) -> SocketAddrV4 {
+        SocketAddrV4::new(Ipv4Addr::LOCALHOST, self.net_port())
+    }
+}
+
+impl Sidechain for PostSetup {
+    const SIDECHAIN_NUMBER: SidechainNumber =
+        SidechainNumber(plain_bitnames::types::THIS_SIDECHAIN);
+
+    /// Path to bitnames_app
+    type Init = PathBuf;
+
+    type SetupError = SetupError;
+
+    async fn setup(
+        bitnames_app_path: Self::Init,
+        post_setup: &EnforcerPostSetup,
+        res_tx: mpsc::UnboundedSender<anyhow::Result<()>>,
+    ) -> Result<Self, Self::SetupError> {
+        let reserved_ports = ReservedPorts::new()?;
+        let bitnames_dir = post_setup.out_dir.path().join(format!(
+            "bitnames-{}-{}",
+            reserved_ports.net.port(),
+            reserved_ports.rpc.port()
+        ));
+        std::fs::create_dir(&bitnames_dir)
+            .map_err(Self::SetupError::CreateBitNamesDir)?;
+        let bitnames_app = BitNamesApp {
+            path: bitnames_app_path,
+            data_dir: bitnames_dir,
+            log_level: Some(tracing::Level::TRACE),
+            mainchain_grpc_port: post_setup
+                .reserved_ports
+                .enforcer_serve_grpc
+                .port(),
+            net_port: reserved_ports.net.port(),
+            rpc_port: reserved_ports.rpc.port(),
+            zmq_port: reserved_ports.zmq.port(),
+        };
+        let bitnames_app_task = bitnames_app
+            .spawn_command_with_args::<String, String, _, _, _>([], [], {
+                let res_tx = res_tx.clone();
+                move |err| {
+                    let _err: Result<(), _> = res_tx.unbounded_send(Err(err));
+                }
+            });
+        tracing::debug!("Started BitNames");
+        sleep(Duration::from_secs(1)).await;
+        let rpc_client = jsonrpsee::http_client::HttpClient::builder()
+            .build(format!("http://127.0.0.1:{}", reserved_ports.rpc.port()))?;
+        tracing::debug!("Generating mnemonic seed phrase");
+        let mnemonic = rpc_client.generate_mnemonic().await?;
+        tracing::debug!("Setting mnemonic seed phrase");
+        let () = rpc_client.set_seed_from_mnemonic(mnemonic).await?;
+        tracing::debug!("Generating deposit address");
+        let deposit_address = rpc_client.get_new_address().await?;
+        Ok(Self {
+            _bitnames_app_task: bitnames_app_task,
+            rpc_client,
+            deposit_address,
+            reserved_ports,
+        })
+    }
+
+    type GetDepositAddressError = std::convert::Infallible;
+
+    async fn get_deposit_address(
+        &self,
+    ) -> Result<String, Self::GetDepositAddressError> {
+        Ok(self.deposit_address.to_string())
+    }
+
+    type ConfirmDepositError = ConfirmDepositError;
+
+    async fn confirm_deposit(
+        &mut self,
+        post_setup: &mut EnforcerPostSetup,
+        address: &str,
+        value: bitcoin::Amount,
+        txid: bitcoin::Txid,
+    ) -> Result<(), Self::ConfirmDepositError> {
+        let is_expected = |utxo: &PointedOutput<FilledOutput>| {
+            utxo.output.address.to_string() == address
+                && match utxo.output.content {
+                    FilledOutputContent::Bitcoin(utxo_value) => {
+                        utxo_value == value
+                    }
+                    FilledOutputContent::BitcoinWithdrawal { .. }
+                    | FilledOutputContent::BitName(_)
+                    | FilledOutputContent::BitNameReservation(_, _) => false,
+                }
+                && match utxo.outpoint {
+                    plain_bitnames::types::OutPoint::Deposit(outpoint) => {
+                        outpoint.txid == txid
+                    }
+                    _ => false,
+                }
+        };
+        let utxos = self.rpc_client.list_utxos().await?;
+        if utxos.iter().any(is_expected) {
+            return Ok(());
+        }
+        tracing::debug!("Deposit not found, BMM 1 block...");
+        let () = self.bmm_single(post_setup).await?;
+        let utxos = self.rpc_client.list_utxos().await?;
+        if utxos.iter().any(is_expected) {
+            Ok(())
+        } else {
+            Err(Self::ConfirmDepositError::DepositNotFound { txid })
+        }
+    }
+
+    type CreateWithdrawalError = CreateWithdrawalError;
+
+    async fn create_withdrawal(
+        &mut self,
+        post_setup: &mut EnforcerPostSetup,
+        receive_address: &bitcoin::Address,
+        value: bitcoin::Amount,
+        fee: bitcoin::Amount,
+    ) -> Result<bip300301_enforcer_lib::types::M6id, Self::CreateWithdrawalError>
+    {
+        let _txid = self
+            .rpc_client
+            .withdraw(
+                receive_address.as_unchecked().clone(),
+                value.to_sat(),
+                0,
+                fee.to_sat(),
+            )
+            .await?;
+        let blocks_to_mine = 'blocks_to_mine: {
+            use plain_bitnames::state::WITHDRAWAL_BUNDLE_FAILURE_GAP;
+            let block_count = self.rpc_client.getblockcount().await?;
+            let Some(block_height) = block_count.checked_sub(1) else {
+                break 'blocks_to_mine WITHDRAWAL_BUNDLE_FAILURE_GAP;
+            };
+            let latest_failed_withdrawal_bundle_height = self
+                .rpc_client
+                .latest_failed_withdrawal_bundle_height()
+                .await?
+                .unwrap_or(0);
+            match WITHDRAWAL_BUNDLE_FAILURE_GAP.saturating_sub(
+                block_height - latest_failed_withdrawal_bundle_height,
+            ) {
+                0 => WITHDRAWAL_BUNDLE_FAILURE_GAP + 1,
+                blocks_to_mine => blocks_to_mine,
+            }
+        };
+        tracing::debug!(
+            "Mining BitNames blocks until withdrawal bundle is broadcast"
+        );
+        let () = self.bmm(post_setup, blocks_to_mine).await?;
+        let pending_withdrawal_bundle =
+            self.rpc_client.pending_withdrawal_bundle().await?.ok_or(
+                Self::CreateWithdrawalError::PendingWithdrawalBundleNotFound,
+            )?;
+        let m6id = pending_withdrawal_bundle.compute_m6id();
+        Ok(bip300301_enforcer_lib::types::M6id(m6id.0))
+    }
+}

integration_tests/util.rs
@@ -1,0 +1,91 @@
+use std::{
+    ffi::{OsStr, OsString},
+    path::PathBuf,
+};
+
+use bip300301_enforcer_integration_tests::util::{
+    get_env_var, spawn_command_with_args, AbortOnDrop,
+    BinPaths as EnforcerBinPaths, VarError,
+};
+
+fn load_env_var_from_string(s: &str) -> Result<(), VarError> {
+    dotenvy::from_read_override(s.as_bytes())
+        .map_err(|err| VarError::new(s, err))
+}
+
+#[derive(Clone, Debug)]
+pub struct BinPaths {
+    pub bitnames: PathBuf,
+    pub others: EnforcerBinPaths,
+}
+
+impl BinPaths {
+    /// Read from environment variables
+    pub fn from_env() -> Result<Self, VarError> {
+        let () = load_env_var_from_string("BITCOIN_UTIL=''")?;
+        let () = load_env_var_from_string("SIGNET_MINER=''")?;
+        Ok(Self {
+            bitnames: get_env_var("BITNAMES_APP")?.into(),
+            others: EnforcerBinPaths::from_env()?,
+        })
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct BitNamesApp {
+    pub path: PathBuf,
+    pub data_dir: PathBuf,
+    pub log_level: Option<tracing::Level>,
+    pub mainchain_grpc_port: u16,
+    /// Port to use for P2P networking
+    pub net_port: u16,
+    /// Port to use for the RPC server
+    pub rpc_port: u16,
+    /// Port to use for ZMQ server
+    pub zmq_port: u16,
+}
+
+impl BitNamesApp {
+    pub fn spawn_command_with_args<Env, Arg, Envs, Args, F>(
+        &self,
+        envs: Envs,
+        args: Args,
+        err_handler: F,
+    ) -> AbortOnDrop<()>
+    where
+        Arg: AsRef<OsStr>,
+        Env: AsRef<OsStr>,
+        Envs: IntoIterator<Item = (Env, Env)>,
+        Args: IntoIterator<Item = Arg>,
+        F: FnOnce(anyhow::Error) + Send + 'static,
+    {
+        let mut default_args = vec![
+            "--datadir".to_owned(),
+            self.data_dir.display().to_string(),
+            "--headless".to_owned(),
+            "--main-addr".to_owned(),
+            format!("127.0.0.1:{}", self.mainchain_grpc_port),
+            "--net-addr".to_owned(),
+            format!("127.0.0.1:{}", self.net_port),
+            "--rpc-addr".to_owned(),
+            format!("127.0.0.1:{}", self.rpc_port),
+            "--zmq-addr".to_owned(),
+            format!("127.0.0.1:{}", self.zmq_port),
+        ];
+        if let Some(log_level) = self.log_level {
+            default_args.push("--log-level".to_owned());
+            default_args.push(log_level.as_str().to_owned());
+        }
+        let args = default_args
+            .into_iter()
+            .map(OsString::from)
+            .chain(args.into_iter().map(|arg| arg.as_ref().to_owned()));
+        spawn_command_with_args(
+            &self.data_dir,
+            self.path.clone(),
+            envs,
+            args,
+            err_handler,
+        )
+    }
+}

LayerTwo-Labs/plain-bitassets

integration_tests/setup.rs
@@ -1,0 +1,290 @@
+use std::{
+    net::{Ipv4Addr, SocketAddrV4},
+    path::PathBuf,
+    time::Duration,
+};
+
+use bip300301_enforcer_integration_tests::{
+    setup::{PostSetup as EnforcerPostSetup, Sidechain},
+    util::AbortOnDrop,
+};
+use bip300301_enforcer_lib::types::SidechainNumber;
+use futures::{channel::mpsc, future, TryFutureExt as _};
+use plain_bitassets::types::{FilledOutputContent, PointedOutput};
+use plain_bitassets_app_rpc_api::RpcClient as _;
+use reserve_port::ReservedPort;
+use thiserror::Error;
+use tokio::time::sleep;
+
+use crate::util::BitAssetsApp;
+
+#[derive(Debug)]
+pub struct ReservedPorts {
+    pub net: ReservedPort,
+    pub rpc: ReservedPort,
+    pub zmq: ReservedPort,
+}
+
+impl ReservedPorts {
+    pub fn new() -> Result<Self, reserve_port::Error> {
+        Ok(Self {
+            net: ReservedPort::random()?,
+            rpc: ReservedPort::random()?,
+            zmq: ReservedPort::random()?,
+        })
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum BmmError {
+    #[error(transparent)]
+    Mine(#[from] bip300301_enforcer_integration_tests::mine::MineError),
+    #[error(transparent)]
+    RpcClient(#[from] jsonrpsee::core::ClientError),
+}
+
+#[derive(Debug, Error)]
+pub enum SetupError {
+    #[error("Failed to create BitAssets dir")]
+    CreateBitAssetsDir(#[source] std::io::Error),
+    #[error(transparent)]
+    ReservePort(#[from] reserve_port::Error),
+    #[error(transparent)]
+    RpcClient(#[from] jsonrpsee::core::ClientError),
+}
+
+#[derive(Debug, Error)]
+pub enum ConfirmDepositError {
+    #[error(transparent)]
+    Bmm(#[from] BmmError),
+    #[error("Deposit not found with txid: `{txid}`")]
+    DepositNotFound { txid: bitcoin::Txid },
+    #[error(transparent)]
+    RpcClient(#[from] jsonrpsee::core::ClientError),
+}
+
+#[derive(Debug, Error)]
+pub enum CreateWithdrawalError {
+    #[error(transparent)]
+    Bmm(#[from] BmmError),
+    #[error("Pending withdrawal bundle not found")]
+    PendingWithdrawalBundleNotFound,
+    #[error(transparent)]
+    RpcClient(#[from] jsonrpsee::core::ClientError),
+}
+
+#[derive(Debug)]
+pub struct PostSetup {
+    // MUST occur before temp dirs and reserved ports in order to ensure that processes are dropped
+    // before reserved ports are freed and temp dirs are cleared
+    pub _bitassets_app_task: AbortOnDrop<()>,
+    /// RPC client for bitassets_app
+    pub rpc_client: jsonrpsee::http_client::HttpClient,
+    /// Address for receiving deposits
+    pub deposit_address: plain_bitassets::types::Address,
+    // MUST occur after tasks in order to ensure that tasks are dropped
+    // before reserved ports are freed
+    pub reserved_ports: ReservedPorts,
+}
+
+impl PostSetup {
+    /// BMM a block
+    pub async fn bmm_single(
+        &self,
+        post_setup: &mut EnforcerPostSetup,
+    ) -> Result<(), BmmError> {
+        use bip300301_enforcer_integration_tests::mine::mine;
+        let ((), ()) = future::try_join(
+            self.rpc_client.mine(None).map_err(BmmError::from),
+            async {
+                sleep(Duration::from_secs(1)).await;
+                mine::<Self>(post_setup, 1, Some(true))
+                    .await
+                    .map_err(BmmError::from)
+            },
+        )
+        .await?;
+        Ok(())
+    }
+
+    /// BMM blocks
+    pub async fn bmm(
+        &self,
+        post_setup: &mut EnforcerPostSetup,
+        blocks: u32,
+    ) -> Result<(), BmmError> {
+        for i in 0..blocks {
+            tracing::debug!("BMM block {}/{blocks}", i + 1);
+            let () = self.bmm_single(post_setup).await?;
+        }
+        Ok(())
+    }
+
+    pub fn net_port(&self) -> u16 {
+        self.reserved_ports.net.port()
+    }
+
+    pub fn net_addr(&self) -> SocketAddrV4 {
+        SocketAddrV4::new(Ipv4Addr::LOCALHOST, self.net_port())
+    }
+}
+
+impl Sidechain for PostSetup {
+    const SIDECHAIN_NUMBER: SidechainNumber =
+        SidechainNumber(plain_bitassets::types::THIS_SIDECHAIN);
+
+    /// Path to bitassets_app
+    type Init = PathBuf;
+
+    type SetupError = SetupError;
+
+    async fn setup(
+        bitassets_app_path: Self::Init,
+        post_setup: &EnforcerPostSetup,
+        res_tx: mpsc::UnboundedSender<anyhow::Result<()>>,
+    ) -> Result<Self, Self::SetupError> {
+        let reserved_ports = ReservedPorts::new()?;
+        let bitassets_dir = post_setup.out_dir.path().join(format!(
+            "bitassets-{}-{}",
+            reserved_ports.net.port(),
+            reserved_ports.rpc.port()
+        ));
+        std::fs::create_dir(&bitassets_dir)
+            .map_err(Self::SetupError::CreateBitAssetsDir)?;
+        let bitassets_app = BitAssetsApp {
+            path: bitassets_app_path,
+            data_dir: bitassets_dir,
+            log_level: Some(tracing::Level::TRACE),
+            mainchain_grpc_port: post_setup
+                .reserved_ports
+                .enforcer_serve_grpc
+                .port(),
+            net_port: reserved_ports.net.port(),
+            rpc_port: reserved_ports.rpc.port(),
+            zmq_port: reserved_ports.zmq.port(),
+        };
+        let bitassets_app_task = bitassets_app
+            .spawn_command_with_args::<String, String, _, _, _>([], [], {
+                let res_tx = res_tx.clone();
+                move |err| {
+                    let _err: Result<(), _> = res_tx.unbounded_send(Err(err));
+                }
+            });
+        tracing::debug!("Started BitAssets");
+        sleep(Duration::from_secs(1)).await;
+        let rpc_client = jsonrpsee::http_client::HttpClient::builder()
+            .build(format!("http://127.0.0.1:{}", reserved_ports.rpc.port()))?;
+        tracing::debug!("Generating mnemonic seed phrase");
+        let mnemonic = rpc_client.generate_mnemonic().await?;
+        tracing::debug!("Setting mnemonic seed phrase");
+        let () = rpc_client.set_seed_from_mnemonic(mnemonic).await?;
+        tracing::debug!("Generating deposit address");
+        let deposit_address = rpc_client.get_new_address().await?;
+        Ok(Self {
+            _bitassets_app_task: bitassets_app_task,
+            rpc_client,
+            deposit_address,
+            reserved_ports,
+        })
+    }
+
+    type GetDepositAddressError = std::convert::Infallible;
+
+    async fn get_deposit_address(
+        &self,
+    ) -> Result<String, Self::GetDepositAddressError> {
+        Ok(self.deposit_address.to_string())
+    }
+
+    type ConfirmDepositError = ConfirmDepositError;
+
+    async fn confirm_deposit(
+        &mut self,
+        post_setup: &mut EnforcerPostSetup,
+        address: &str,
+        value: bitcoin::Amount,
+        txid: bitcoin::Txid,
+    ) -> Result<(), Self::ConfirmDepositError> {
+        let is_expected = |utxo: &PointedOutput<FilledOutputContent>| {
+            utxo.output.address.to_string() == address
+                && match utxo.output.content {
+                    FilledOutputContent::Bitcoin(utxo_value) => {
+                        utxo_value.0 == value
+                    }
+                    FilledOutputContent::AmmLpToken { .. }
+                    | FilledOutputContent::BitcoinWithdrawal { .. }
+                    | FilledOutputContent::BitAsset(..)
+                    | FilledOutputContent::BitAssetControl(_)
+                    | FilledOutputContent::BitAssetReservation(_, _)
+                    | FilledOutputContent::DutchAuctionReceipt(_) => false,
+                }
+                && match utxo.outpoint {
+                    plain_bitassets::types::OutPoint::Deposit(outpoint) => {
+                        outpoint.txid == txid
+                    }
+                    _ => false,
+                }
+        };
+        let utxos = self.rpc_client.list_utxos().await?;
+        if utxos.iter().any(is_expected) {
+            return Ok(());
+        }
+        tracing::debug!("Deposit not found, BMM 1 block...");
+        let () = self.bmm_single(post_setup).await?;
+        let utxos = self.rpc_client.list_utxos().await?;
+        if utxos.iter().any(is_expected) {
+            Ok(())
+        } else {
+            Err(Self::ConfirmDepositError::DepositNotFound { txid })
+        }
+    }
+
+    type CreateWithdrawalError = CreateWithdrawalError;
+
+    async fn create_withdrawal(
+        &mut self,
+        post_setup: &mut EnforcerPostSetup,
+        receive_address: &bitcoin::Address,
+        value: bitcoin::Amount,
+        fee: bitcoin::Amount,
+    ) -> Result<bip300301_enforcer_lib::types::M6id, Self::CreateWithdrawalError>
+    {
+        let _txid = self
+            .rpc_client
+            .withdraw(
+                receive_address.as_unchecked().clone(),
+                value.to_sat(),
+                0,
+                fee.to_sat(),
+            )
+            .await?;
+        let blocks_to_mine = 'blocks_to_mine: {
+            use plain_bitassets::state::WITHDRAWAL_BUNDLE_FAILURE_GAP;
+            let block_count = self.rpc_client.getblockcount().await?;
+            let Some(block_height) = block_count.checked_sub(1) else {
+                break 'blocks_to_mine WITHDRAWAL_BUNDLE_FAILURE_GAP;
+            };
+            let latest_failed_withdrawal_bundle_height = self
+                .rpc_client
+                .latest_failed_withdrawal_bundle_height()
+                .await?
+                .unwrap_or(0);
+            match WITHDRAWAL_BUNDLE_FAILURE_GAP.saturating_sub(
+                block_height - latest_failed_withdrawal_bundle_height,
+            ) {
+                0 => WITHDRAWAL_BUNDLE_FAILURE_GAP + 1,
+                blocks_to_mine => blocks_to_mine,
+            }
+        };
+        tracing::debug!(
+            "Mining BitAssets blocks until withdrawal bundle is broadcast"
+        );
+        let () = self.bmm(post_setup, blocks_to_mine).await?;
+        let pending_withdrawal_bundle =
+            self.rpc_client.pending_withdrawal_bundle().await?.ok_or(
+                Self::CreateWithdrawalError::PendingWithdrawalBundleNotFound,
+            )?;
+        let m6id = pending_withdrawal_bundle.compute_m6id();
+        Ok(bip300301_enforcer_lib::types::M6id(m6id.0))
+    }
+}

integration_tests/util.rs
@@ -1,0 +1,91 @@
+use std::{
+    ffi::{OsStr, OsString},
+    path::PathBuf,
+};
+
+use bip300301_enforcer_integration_tests::util::{
+    get_env_var, spawn_command_with_args, AbortOnDrop,
+    BinPaths as EnforcerBinPaths, VarError,
+};
+
+fn load_env_var_from_string(s: &str) -> Result<(), VarError> {
+    dotenvy::from_read_override(s.as_bytes())
+        .map_err(|err| VarError::new(s, err))
+}
+
+#[derive(Clone, Debug)]
+pub struct BinPaths {
+    pub bitassets: PathBuf,
+    pub others: EnforcerBinPaths,
+}
+
+impl BinPaths {
+    /// Read from environment variables
+    pub fn from_env() -> Result<Self, VarError> {
+        let () = load_env_var_from_string("BITCOIN_UTIL=''")?;
+        let () = load_env_var_from_string("SIGNET_MINER=''")?;
+        Ok(Self {
+            bitassets: get_env_var("BITASSETS_APP")?.into(),
+            others: EnforcerBinPaths::from_env()?,
+        })
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct BitAssetsApp {
+    pub path: PathBuf,
+    pub data_dir: PathBuf,
+    pub log_level: Option<tracing::Level>,
+    pub mainchain_grpc_port: u16,
+    /// Port to use for P2P networking
+    pub net_port: u16,
+    /// Port to use for the RPC server
+    pub rpc_port: u16,
+    /// Port to use for ZMQ server
+    pub zmq_port: u16,
+}
+
+impl BitAssetsApp {
+    pub fn spawn_command_with_args<Env, Arg, Envs, Args, F>(
+        &self,
+        envs: Envs,
+        args: Args,
+        err_handler: F,
+    ) -> AbortOnDrop<()>
+    where
+        Arg: AsRef<OsStr>,
+        Env: AsRef<OsStr>,
+        Envs: IntoIterator<Item = (Env, Env)>,
+        Args: IntoIterator<Item = Arg>,
+        F: FnOnce(anyhow::Error) + Send + 'static,
+    {
+        let mut default_args = vec![
+            "--datadir".to_owned(),
+            self.data_dir.display().to_string(),
+            "--headless".to_owned(),
+            "--main-addr".to_owned(),
+            format!("127.0.0.1:{}", self.mainchain_grpc_port),
+            "--net-addr".to_owned(),
+            format!("127.0.0.1:{}", self.net_port),
+            "--rpc-addr".to_owned(),
+            format!("127.0.0.1:{}", self.rpc_port),
+            "--zmq-addr".to_owned(),
+            format!("127.0.0.1:{}", self.zmq_port),
+        ];
+        if let Some(log_level) = self.log_level {
+            default_args.push("--log-level".to_owned());
+            default_args.push(log_level.as_str().to_owned());
+        }
+        let args = default_args
+            .into_iter()
+            .map(OsString::from)
+            .chain(args.into_iter().map(|arg| arg.as_ref().to_owned()));
+        spawn_command_with_args(
+            &self.data_dir,
+            self.path.clone(),
+            envs,
+            args,
+            err_handler,
+        )
+    }
+}