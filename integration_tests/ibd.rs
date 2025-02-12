LayerTwo-Labs/plain-bitnames

Add integration test for unknown withdrawals

integration_tests/ibd.rs
@@ -15,7 +15,10 @@ use plain_bitnames_app_rpc_api::RpcClient as _;
 use tokio::time::sleep;
 use tracing::Instrument as _;
 
-use crate::{setup::PostSetup, util::BinPaths};
+use crate::{
+    setup::{Init, PostSetup},
+    util::BinPaths,
+};
 
 #[derive(Debug)]
 struct BitNamesNodes {
@@ -38,15 +41,24 @@ async fn setup(
     )
     .await?;
     let sidechain_sender = PostSetup::setup(
-        bin_paths.bitnames.clone(),
+        Init {
+            bitnames_app: bin_paths.bitnames.clone(),
+            data_dir_suffix: Some("sender".to_owned()),
+        },
         &enforcer_post_setup,
         res_tx.clone(),
     )
     .await?;
     tracing::info!("Setup BitNames send node successfully");
-    let sidechain_syncer =
-        PostSetup::setup(bin_paths.bitnames, &enforcer_post_setup, res_tx)
-            .await?;
+    let sidechain_syncer = PostSetup::setup(
+        Init {
+            bitnames_app: bin_paths.bitnames.clone(),
+            data_dir_suffix: Some("syncer".to_owned()),
+        },
+        &enforcer_post_setup,
+        res_tx,
+    )
+    .await?;
     tracing::info!("Setup BitNames sync node successfully");
     let bitnames_nodes = BitNamesNodes {
         sender: sidechain_sender,

integration_tests/setup.rs
@@ -35,6 +35,12 @@ impl ReservedPorts {
     }
 }
 
+#[derive(Debug)]
+pub struct Init {
+    pub bitnames_app: PathBuf,
+    pub data_dir_suffix: Option<String>,
+}
+
 #[derive(Debug, Error)]
 pub enum BmmError {
     #[error(transparent)]
@@ -133,26 +139,25 @@ impl Sidechain for PostSetup {
     const SIDECHAIN_NUMBER: SidechainNumber =
         SidechainNumber(plain_bitnames::types::THIS_SIDECHAIN);
 
-    /// Path to bitnames_app
-    type Init = PathBuf;
+    type Init = Init;
 
     type SetupError = SetupError;
 
     async fn setup(
-        bitnames_app_path: Self::Init,
+        init: Self::Init,
         post_setup: &EnforcerPostSetup,
         res_tx: mpsc::UnboundedSender<anyhow::Result<()>>,
     ) -> Result<Self, Self::SetupError> {
         let reserved_ports = ReservedPorts::new()?;
-        let bitnames_dir = post_setup.out_dir.path().join(format!(
-            "bitnames-{}-{}",
-            reserved_ports.net.port(),
-            reserved_ports.rpc.port()
-        ));
+        let bitnames_dir = if let Some(suffix) = init.data_dir_suffix {
+            post_setup.out_dir.path().join(format!("bitnames-{suffix}"))
+        } else {
+            post_setup.out_dir.path().join("bitnames")
+        };
         std::fs::create_dir(&bitnames_dir)
             .map_err(Self::SetupError::CreateBitNamesDir)?;
         let bitnames_app = BitNamesApp {
-            path: bitnames_app_path,
+            path: init.bitnames_app,
             data_dir: bitnames_dir,
             log_level: Some(tracing::Level::TRACE),
             mainchain_grpc_port: post_setup

integration_tests/unknown_withdrawal.rs
@@ -1,0 +1,164 @@
+//! Test an unknown withdrawal event
+
+use bip300301_enforcer_integration_tests::{
+    integration_test::{
+        activate_sidechain, deposit, fund_enforcer, propose_sidechain,
+        withdraw_succeed,
+    },
+    setup::{
+        setup as setup_enforcer, Mode, Network, PostSetup as EnforcerPostSetup,
+        Sidechain as _,
+    },
+    util::{AbortOnDrop, AsyncTrial},
+};
+use futures::{
+    channel::mpsc, future::BoxFuture, FutureExt as _, StreamExt as _,
+};
+use plain_bitnames_app_rpc_api::RpcClient as _;
+use tokio::time::sleep;
+use tracing::Instrument as _;
+
+use crate::{
+    setup::{Init, PostSetup},
+    util::BinPaths,
+};
+
+/// Initial setup for the test
+async fn setup(
+    bin_paths: &BinPaths,
+    res_tx: mpsc::UnboundedSender<anyhow::Result<()>>,
+) -> anyhow::Result<EnforcerPostSetup> {
+    let mut enforcer_post_setup = setup_enforcer(
+        &bin_paths.others,
+        Network::Regtest,
+        Mode::Mempool,
+        res_tx.clone(),
+    )
+    .await?;
+    let () = propose_sidechain::<PostSetup>(&mut enforcer_post_setup).await?;
+    tracing::info!("Proposed sidechain successfully");
+    let () = activate_sidechain::<PostSetup>(&mut enforcer_post_setup).await?;
+    tracing::info!("Activated sidechain successfully");
+    let () = fund_enforcer::<PostSetup>(&mut enforcer_post_setup).await?;
+    Ok(enforcer_post_setup)
+}
+
+const DEPOSIT_AMOUNT: bitcoin::Amount = bitcoin::Amount::from_sat(21_000_000);
+const DEPOSIT_FEE: bitcoin::Amount = bitcoin::Amount::from_sat(1_000_000);
+const WITHDRAW_AMOUNT: bitcoin::Amount = bitcoin::Amount::from_sat(18_000_000);
+const WITHDRAW_FEE: bitcoin::Amount = bitcoin::Amount::from_sat(1_000_000);
+
+async fn unknown_withdrawal_task(
+    bin_paths: BinPaths,
+    res_tx: mpsc::UnboundedSender<anyhow::Result<()>>,
+) -> anyhow::Result<()> {
+    let mut enforcer_post_setup = setup(&bin_paths, res_tx.clone()).await?;
+    let mut sidechain_withdrawer = PostSetup::setup(
+        Init {
+            bitnames_app: bin_paths.bitnames.clone(),
+            data_dir_suffix: Some("withdrawer".to_owned()),
+        },
+        &enforcer_post_setup,
+        res_tx.clone(),
+    )
+    .await?;
+    tracing::info!("Setup BitNames withdrawer node successfully");
+    let withdrawer_deposit_address =
+        sidechain_withdrawer.get_deposit_address().await?;
+    let () = deposit(
+        &mut enforcer_post_setup,
+        &mut sidechain_withdrawer,
+        &withdrawer_deposit_address,
+        DEPOSIT_AMOUNT,
+        DEPOSIT_FEE,
+    )
+    .await?;
+    tracing::info!("Deposited to sidechain successfully");
+    let () = withdraw_succeed(
+        &mut enforcer_post_setup,
+        &mut sidechain_withdrawer,
+        WITHDRAW_AMOUNT,
+        WITHDRAW_FEE,
+        bitcoin::Amount::ZERO,
+    )
+    .await?;
+    tracing::info!("Withdrawal succeeded");
+    // New sidechain node, starting from scratch
+    let mut sidechain_successor = PostSetup::setup(
+        Init {
+            bitnames_app: bin_paths.bitnames,
+            data_dir_suffix: Some("successor".to_owned()),
+        },
+        &enforcer_post_setup,
+        res_tx,
+    )
+    .await?;
+    tracing::info!("Setup BitNames successor node successfully");
+    tracing::debug!("BMM 1 block");
+    sidechain_successor
+        .bmm_single(&mut enforcer_post_setup)
+        .await?;
+    tracing::debug!("Checking that successor sidechain has exactly 1 block");
+    let successor_block_count =
+        sidechain_successor.rpc_client.getblockcount().await?;
+    anyhow::ensure!(successor_block_count == 1);
+    tracing::debug!("Checking that successor sidechain has no deposit UTXOs");
+    let successor_utxos = sidechain_successor.rpc_client.list_utxos().await?;
+    anyhow::ensure!(successor_utxos.is_empty());
+    let successor_deposit_address =
+        sidechain_successor.get_deposit_address().await?;
+    let () = deposit(
+        &mut enforcer_post_setup,
+        &mut sidechain_successor,
+        &successor_deposit_address,
+        DEPOSIT_AMOUNT,
+        DEPOSIT_FEE,
+    )
+    .await?;
+    tracing::info!("Deposited to sidechain successfully");
+    tracing::debug!("Checking that withdrawer sidechain recognizes deposit");
+    {
+        let withdrawer_utxos_count =
+            sidechain_withdrawer.rpc_client.list_utxos().await?.len();
+        sidechain_withdrawer
+            .bmm_single(&mut enforcer_post_setup)
+            .await?;
+        let utxos_count_delta = sidechain_withdrawer
+            .rpc_client
+            .list_utxos()
+            .await?
+            .len()
+            .checked_sub(withdrawer_utxos_count);
+        anyhow::ensure!(utxos_count_delta == Some(1));
+    }
+    drop(sidechain_successor);
+    drop(sidechain_withdrawer);
+    tracing::info!("Removing {}", enforcer_post_setup.out_dir.path().display());
+    drop(enforcer_post_setup.tasks);
+    // Wait for tasks to die
+    sleep(std::time::Duration::from_secs(1)).await;
+    enforcer_post_setup.out_dir.cleanup()?;
+    Ok(())
+}
+
+async fn unknown_withdrawal(bin_paths: BinPaths) -> anyhow::Result<()> {
+    let (res_tx, mut res_rx) = mpsc::unbounded();
+    let _test_task: AbortOnDrop<()> = tokio::task::spawn({
+        let res_tx = res_tx.clone();
+        async move {
+            let res = unknown_withdrawal_task(bin_paths, res_tx.clone()).await;
+            let _send_err: Result<(), _> = res_tx.unbounded_send(res);
+        }
+        .in_current_span()
+    })
+    .into();
+    res_rx.next().await.ok_or_else(|| {
+        anyhow::anyhow!("Unexpected end of test task result stream")
+    })?
+}
+
+pub fn unknown_withdrawal_trial(
+    bin_paths: BinPaths,
+) -> AsyncTrial<BoxFuture<'static, anyhow::Result<()>>> {
+    AsyncTrial::new("unknown_withdrawal", unknown_withdrawal(bin_paths).boxed())
+}

LayerTwo-Labs/plain-bitassets

integration_tests/ibd.rs
@@ -15,7 +15,10 @@ use plain_bitassets_app_rpc_api::RpcClient as _;
 use tokio::time::sleep;
 use tracing::Instrument as _;
 
-use crate::{setup::PostSetup, util::BinPaths};
+use crate::{
+    setup::{Init, PostSetup},
+    util::BinPaths,
+};
 
 #[derive(Debug)]
 struct BitAssetsNodes {
@@ -38,15 +41,24 @@ async fn setup(
     )
     .await?;
     let sidechain_sender = PostSetup::setup(
-        bin_paths.bitassets.clone(),
+        Init {
+            bitassets_app: bin_paths.bitassets.clone(),
+            data_dir_suffix: Some("sender".to_owned()),
+        },
         &enforcer_post_setup,
         res_tx.clone(),
     )
     .await?;
     tracing::info!("Setup BitAssets send node successfully");
-    let sidechain_syncer =
-        PostSetup::setup(bin_paths.bitassets, &enforcer_post_setup, res_tx)
-            .await?;
+    let sidechain_syncer = PostSetup::setup(
+        Init {
+            bitassets_app: bin_paths.bitassets.clone(),
+            data_dir_suffix: Some("syncer".to_owned()),
+        },
+        &enforcer_post_setup,
+        res_tx,
+    )
+    .await?;
     tracing::info!("Setup BitAssets sync node successfully");
     let bitassets_nodes = BitAssetsNodes {
         sender: sidechain_sender,

integration_tests/setup.rs
@@ -35,6 +35,12 @@ impl ReservedPorts {
     }
 }
 
+#[derive(Debug)]
+pub struct Init {
+    pub bitassets_app: PathBuf,
+    pub data_dir_suffix: Option<String>,
+}
+
 #[derive(Debug, Error)]
 pub enum BmmError {
     #[error(transparent)]
@@ -133,26 +139,28 @@ impl Sidechain for PostSetup {
     const SIDECHAIN_NUMBER: SidechainNumber =
         SidechainNumber(plain_bitassets::types::THIS_SIDECHAIN);
 
-    /// Path to bitassets_app
-    type Init = PathBuf;
+    type Init = Init;
 
     type SetupError = SetupError;
 
     async fn setup(
-        bitassets_app_path: Self::Init,
+        init: Self::Init,
         post_setup: &EnforcerPostSetup,
         res_tx: mpsc::UnboundedSender<anyhow::Result<()>>,
     ) -> Result<Self, Self::SetupError> {
         let reserved_ports = ReservedPorts::new()?;
-        let bitassets_dir = post_setup.out_dir.path().join(format!(
-            "bitassets-{}-{}",
-            reserved_ports.net.port(),
-            reserved_ports.rpc.port()
-        ));
+        let bitassets_dir = if let Some(suffix) = init.data_dir_suffix {
+            post_setup
+                .out_dir
+                .path()
+                .join(format!("bitassets-{suffix}"))
+        } else {
+            post_setup.out_dir.path().join("bitassets")
+        };
         std::fs::create_dir(&bitassets_dir)
             .map_err(Self::SetupError::CreateBitAssetsDir)?;
         let bitassets_app = BitAssetsApp {
-            path: bitassets_app_path,
+            path: init.bitassets_app,
             data_dir: bitassets_dir,
             log_level: Some(tracing::Level::TRACE),
             mainchain_grpc_port: post_setup

integration_tests/unknown_withdrawal.rs
@@ -1,0 +1,164 @@
+//! Test an unknown withdrawal event
+
+use bip300301_enforcer_integration_tests::{
+    integration_test::{
+        activate_sidechain, deposit, fund_enforcer, propose_sidechain,
+        withdraw_succeed,
+    },
+    setup::{
+        setup as setup_enforcer, Mode, Network, PostSetup as EnforcerPostSetup,
+        Sidechain as _,
+    },
+    util::{AbortOnDrop, AsyncTrial},
+};
+use futures::{
+    channel::mpsc, future::BoxFuture, FutureExt as _, StreamExt as _,
+};
+use plain_bitassets_app_rpc_api::RpcClient as _;
+use tokio::time::sleep;
+use tracing::Instrument as _;
+
+use crate::{
+    setup::{Init, PostSetup},
+    util::BinPaths,
+};
+
+/// Initial setup for the test
+async fn setup(
+    bin_paths: &BinPaths,
+    res_tx: mpsc::UnboundedSender<anyhow::Result<()>>,
+) -> anyhow::Result<EnforcerPostSetup> {
+    let mut enforcer_post_setup = setup_enforcer(
+        &bin_paths.others,
+        Network::Regtest,
+        Mode::Mempool,
+        res_tx.clone(),
+    )
+    .await?;
+    let () = propose_sidechain::<PostSetup>(&mut enforcer_post_setup).await?;
+    tracing::info!("Proposed sidechain successfully");
+    let () = activate_sidechain::<PostSetup>(&mut enforcer_post_setup).await?;
+    tracing::info!("Activated sidechain successfully");
+    let () = fund_enforcer::<PostSetup>(&mut enforcer_post_setup).await?;
+    Ok(enforcer_post_setup)
+}
+
+const DEPOSIT_AMOUNT: bitcoin::Amount = bitcoin::Amount::from_sat(21_000_000);
+const DEPOSIT_FEE: bitcoin::Amount = bitcoin::Amount::from_sat(1_000_000);
+const WITHDRAW_AMOUNT: bitcoin::Amount = bitcoin::Amount::from_sat(18_000_000);
+const WITHDRAW_FEE: bitcoin::Amount = bitcoin::Amount::from_sat(1_000_000);
+
+async fn unknown_withdrawal_task(
+    bin_paths: BinPaths,
+    res_tx: mpsc::UnboundedSender<anyhow::Result<()>>,
+) -> anyhow::Result<()> {
+    let mut enforcer_post_setup = setup(&bin_paths, res_tx.clone()).await?;
+    let mut sidechain_withdrawer = PostSetup::setup(
+        Init {
+            bitassets_app: bin_paths.bitassets.clone(),
+            data_dir_suffix: Some("withdrawer".to_owned()),
+        },
+        &enforcer_post_setup,
+        res_tx.clone(),
+    )
+    .await?;
+    tracing::info!("Setup BitAssets withdrawer node successfully");
+    let withdrawer_deposit_address =
+        sidechain_withdrawer.get_deposit_address().await?;
+    let () = deposit(
+        &mut enforcer_post_setup,
+        &mut sidechain_withdrawer,
+        &withdrawer_deposit_address,
+        DEPOSIT_AMOUNT,
+        DEPOSIT_FEE,
+    )
+    .await?;
+    tracing::info!("Deposited to sidechain successfully");
+    let () = withdraw_succeed(
+        &mut enforcer_post_setup,
+        &mut sidechain_withdrawer,
+        WITHDRAW_AMOUNT,
+        WITHDRAW_FEE,
+        bitcoin::Amount::ZERO,
+    )
+    .await?;
+    tracing::info!("Withdrawal succeeded");
+    // New sidechain node, starting from scratch
+    let mut sidechain_successor = PostSetup::setup(
+        Init {
+            bitassets_app: bin_paths.bitassets,
+            data_dir_suffix: Some("successor".to_owned()),
+        },
+        &enforcer_post_setup,
+        res_tx,
+    )
+    .await?;
+    tracing::info!("Setup BitAssets successor node successfully");
+    tracing::debug!("BMM 1 block");
+    sidechain_successor
+        .bmm_single(&mut enforcer_post_setup)
+        .await?;
+    tracing::debug!("Checking that successor sidechain has exactly 1 block");
+    let successor_block_count =
+        sidechain_successor.rpc_client.getblockcount().await?;
+    anyhow::ensure!(successor_block_count == 1);
+    tracing::debug!("Checking that successor sidechain has no deposit UTXOs");
+    let successor_utxos = sidechain_successor.rpc_client.list_utxos().await?;
+    anyhow::ensure!(successor_utxos.is_empty());
+    let successor_deposit_address =
+        sidechain_successor.get_deposit_address().await?;
+    let () = deposit(
+        &mut enforcer_post_setup,
+        &mut sidechain_successor,
+        &successor_deposit_address,
+        DEPOSIT_AMOUNT,
+        DEPOSIT_FEE,
+    )
+    .await?;
+    tracing::info!("Deposited to sidechain successfully");
+    tracing::debug!("Checking that withdrawer sidechain recognizes deposit");
+    {
+        let withdrawer_utxos_count =
+            sidechain_withdrawer.rpc_client.list_utxos().await?.len();
+        sidechain_withdrawer
+            .bmm_single(&mut enforcer_post_setup)
+            .await?;
+        let utxos_count_delta = sidechain_withdrawer
+            .rpc_client
+            .list_utxos()
+            .await?
+            .len()
+            .checked_sub(withdrawer_utxos_count);
+        anyhow::ensure!(utxos_count_delta == Some(1));
+    }
+    drop(sidechain_successor);
+    drop(sidechain_withdrawer);
+    tracing::info!("Removing {}", enforcer_post_setup.out_dir.path().display());
+    drop(enforcer_post_setup.tasks);
+    // Wait for tasks to die
+    sleep(std::time::Duration::from_secs(1)).await;
+    enforcer_post_setup.out_dir.cleanup()?;
+    Ok(())
+}
+
+async fn unknown_withdrawal(bin_paths: BinPaths) -> anyhow::Result<()> {
+    let (res_tx, mut res_rx) = mpsc::unbounded();
+    let _test_task: AbortOnDrop<()> = tokio::task::spawn({
+        let res_tx = res_tx.clone();
+        async move {
+            let res = unknown_withdrawal_task(bin_paths, res_tx.clone()).await;
+            let _send_err: Result<(), _> = res_tx.unbounded_send(res);
+        }
+        .in_current_span()
+    })
+    .into();
+    res_rx.next().await.ok_or_else(|| {
+        anyhow::anyhow!("Unexpected end of test task result stream")
+    })?
+}
+
+pub fn unknown_withdrawal_trial(
+    bin_paths: BinPaths,
+) -> AsyncTrial<BoxFuture<'static, anyhow::Result<()>>> {
+    AsyncTrial::new("unknown_withdrawal", unknown_withdrawal(bin_paths).boxed())
+}