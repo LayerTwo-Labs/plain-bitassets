LayerTwo-Labs/plain-bitnames

multi: refactor tokio runtime handling, listen for multiple signals

app/main.rs
@@ -1,9 +1,10 @@
 #![feature(let_chains)]
 #![feature(try_find)]
 
-use std::{path::Path, sync::mpsc};
+use std::path::Path;
 
 use clap::Parser as _;
+use tokio::{signal::ctrl_c, sync::oneshot};
 use tracing_subscriber::{
     filter as tracing_filter, layer::SubscriberExt, Layer,
 };
@@ -137,6 +138,26 @@ fn set_tracing_subscriber(
     Ok((line_buffer, rolling_log_guard))
 }
 
+fn run_egui_app(
+    config: &crate::cli::Config,
+    line_buffer: LineBuffer,
+    app: Option<crate::app::App>,
+) -> Result<(), eframe::Error> {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Plain Bitnames",
+        native_options,
+        Box::new(move |cc| {
+            Ok(Box::new(gui::EguiApp::new(
+                app,
+                cc,
+                line_buffer,
+                config.rpc_addr,
+            )))
+        }),
+    )
+}
+
 fn main() -> anyhow::Result<()> {
     let cli = cli::Cli::parse();
     let config = cli.get_config()?;
@@ -145,55 +166,47 @@ fn main() -> anyhow::Result<()> {
         config.log_dir.as_deref(),
         config.log_level,
     )?;
-    let app: Result<app::App, app::Error> = app::App::new(&config)
-        .inspect(|app| {
-            // spawn rpc server
-            app.runtime.spawn({
-                let app = app.clone();
-                async move {
-                    rpc_server::run_server(app, config.rpc_addr).await.unwrap()
+    let (app_tx, app_rx) = oneshot::channel::<anyhow::Error>();
+    let app = app::App::new(&config).inspect(|app| {
+        // spawn rpc server
+        app.runtime.spawn({
+            let app = app.clone();
+            async move {
+                tracing::info!("starting RPC server at `{}`", config.rpc_addr);
+                if let Err(err) =
+                    rpc_server::run_server(app, config.rpc_addr).await
+                {
+                    app_tx.send(err).expect("failed to send error to app");
                 }
-            });
-        })
-        .inspect_err(|err| {
-            tracing::error!("application error: {:?}", err);
+            }
         });
-
-    if config.headless {
-        tracing::info!("Running in headless mode");
-        drop(line_buffer);
-        let _app = app?;
-        // wait for ctrlc signal
-        let (tx, rx) = mpsc::channel();
-        ctrlc::set_handler(move || {
-            tx.send(()).unwrap();
-        })
-        .expect("Error setting Ctrl-C handler");
-        rx.recv().unwrap();
-        tracing::info!("Received Ctrl-C signal, exiting...");
-    } else {
-        let native_options = eframe::NativeOptions::default();
-        let app: Option<_> = app.map_or_else(
-            |err| {
+    });
+    if !config.headless {
+        let app = match app {
+            Ok(app) => Some(app),
+            Err(err) => {
                 let err = anyhow::Error::from(err);
                 tracing::error!("{err:#}");
                 None
-            },
-            Some,
-        );
-        eframe::run_native(
-            "Plain Bitnames",
-            native_options,
-            Box::new(move |cc| {
-                Ok(Box::new(gui::EguiApp::new(
-                    app,
-                    cc,
-                    line_buffer,
-                    config.rpc_addr,
-                )))
-            }),
-        )
-        .map_err(|err| anyhow::anyhow!("failed to launch egui app: {err}"))?
-    }
-    Ok(())
+            }
+        };
+        // For GUI mode we want the GUI to start, even if the app fails to start.
+        return run_egui_app(&config, line_buffer, app)
+            .map_err(|e| anyhow::anyhow!("failed to run egui app: {e:#}"));
+    };
+    tracing::info!("Running in headless mode");
+    drop(line_buffer);
+    // If we're headless, we want to exit hard if the app fails to start.
+    let app = app?;
+    app.runtime.block_on(async move {
+        tokio::select! {
+            Ok(_) = ctrl_c() => {
+                tracing::info!("Shutting down due to process interruption");
+                Ok(())
+            }
+            Ok(err) = app_rx => {
+                Err(anyhow::anyhow!("received error from RPC server: {err:#} ({err:?})"))
+            }
+        }
+    })
 }

LayerTwo-Labs/plain-bitassets

app/main.rs
@@ -1,9 +1,10 @@
 #![feature(let_chains)]
 #![feature(try_find)]
 
-use std::{path::Path, sync::mpsc};
+use std::path::Path;
 
 use clap::Parser as _;
+use tokio::{signal::ctrl_c, sync::oneshot};
 use tracing_subscriber::{
     filter as tracing_filter, layer::SubscriberExt, Layer,
 };
@@ -137,6 +138,26 @@ fn set_tracing_subscriber(
     Ok((line_buffer, rolling_log_guard))
 }
 
+fn run_egui_app(
+    config: &crate::cli::Config,
+    line_buffer: LineBuffer,
+    app: Option<crate::app::App>,
+) -> Result<(), eframe::Error> {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Plain Bitassets",
+        native_options,
+        Box::new(move |cc| {
+            Ok(Box::new(gui::EguiApp::new(
+                app,
+                cc,
+                line_buffer,
+                config.rpc_addr,
+            )))
+        }),
+    )
+}
+
 fn main() -> anyhow::Result<()> {
     let cli = cli::Cli::parse();
     let config = cli.get_config()?;
@@ -145,55 +166,47 @@ fn main() -> anyhow::Result<()> {
         config.log_dir.as_deref(),
         config.log_level,
     )?;
-    let app: Result<app::App, app::Error> = app::App::new(&config)
-        .inspect(|app| {
-            // spawn rpc server
-            app.runtime.spawn({
-                let app = app.clone();
-                async move {
-                    rpc_server::run_server(app, config.rpc_addr).await.unwrap()
+    let (app_tx, app_rx) = oneshot::channel::<anyhow::Error>();
+    let app = app::App::new(&config).inspect(|app| {
+        // spawn rpc server
+        app.runtime.spawn({
+            let app = app.clone();
+            async move {
+                tracing::info!("starting RPC server at `{}`", config.rpc_addr);
+                if let Err(err) =
+                    rpc_server::run_server(app, config.rpc_addr).await
+                {
+                    app_tx.send(err).expect("failed to send error to app");
                 }
-            });
-        })
-        .inspect_err(|err| {
-            tracing::error!("application error: {:?}", err);
+            }
         });
-
-    if config.headless {
-        tracing::info!("Running in headless mode");
-        drop(line_buffer);
-        let _app = app?;
-        // wait for ctrlc signal
-        let (tx, rx) = mpsc::channel();
-        ctrlc::set_handler(move || {
-            tx.send(()).unwrap();
-        })
-        .expect("Error setting Ctrl-C handler");
-        rx.recv().unwrap();
-        tracing::info!("Received Ctrl-C signal, exiting...");
-    } else {
-        let native_options = eframe::NativeOptions::default();
-        let app: Option<_> = app.map_or_else(
-            |err| {
+    });
+    if !config.headless {
+        let app = match app {
+            Ok(app) => Some(app),
+            Err(err) => {
                 let err = anyhow::Error::from(err);
                 tracing::error!("{err:#}");
                 None
-            },
-            Some,
-        );
-        eframe::run_native(
-            "Plain Bitassets",
-            native_options,
-            Box::new(move |cc| {
-                Ok(Box::new(gui::EguiApp::new(
-                    app,
-                    cc,
-                    line_buffer,
-                    config.rpc_addr,
-                )))
-            }),
-        )
-        .map_err(|err| anyhow::anyhow!("failed to launch egui app: {err}"))?
-    }
-    Ok(())
+            }
+        };
+        // For GUI mode we want the GUI to start, even if the app fails to start.
+        return run_egui_app(&config, line_buffer, app)
+            .map_err(|e| anyhow::anyhow!("failed to run egui app: {e:#}"));
+    };
+    tracing::info!("Running in headless mode");
+    drop(line_buffer);
+    // If we're headless, we want to exit hard if the app fails to start.
+    let app = app?;
+    app.runtime.block_on(async move {
+        tokio::select! {
+            Ok(_) = ctrl_c() => {
+                tracing::info!("Shutting down due to process interruption");
+                Ok(())
+            }
+            Ok(err) = app_rx => {
+                Err(anyhow::anyhow!("received error from RPC server: {err:#} ({err:?})"))
+            }
+        }
+    })
 }