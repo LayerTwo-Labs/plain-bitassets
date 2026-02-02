LayerTwo-Labs/plain-bitnames

Update deps, clippy fixes

integration_tests/integration_test.rs
@@ -1,6 +1,6 @@
 use bip300301_enforcer_integration_tests::{
     setup::{Mode, Network},
-    util::AsyncTrial,
+    util::{AsyncTrial, TestFailureCollector, TestFileRegistry},
 };
 use futures::{FutureExt, future::BoxFuture};
 
@@ -14,25 +14,53 @@ use crate::{
 
 fn deposit_withdraw_roundtrip(
     bin_paths: BinPaths,
+    file_registry: TestFileRegistry,
+    failure_collector: TestFailureCollector,
 ) -> AsyncTrial<BoxFuture<'static, anyhow::Result<()>>> {
-    AsyncTrial::new("deposit_withdraw_roundtrip", async move {
-        bip300301_enforcer_integration_tests::integration_test::deposit_withdraw_roundtrip::<PostSetup>(
-            bin_paths.others, Network::Regtest, Mode::Mempool,
-            Init {
-                bitnames_app: bin_paths.bitnames,
-                data_dir_suffix: None,
-            },
-        ).await
-    }.boxed())
+    AsyncTrial::new(
+        "deposit_withdraw_roundtrip",
+        async move {
+            let (res_tx, _) = futures::channel::mpsc::unbounded();
+            let post_setup = bip300301_enforcer_integration_tests::setup::setup(
+                &bin_paths.others,
+                Network::Regtest,
+                Mode::Mempool,
+                res_tx
+            ).await?;
+            bip300301_enforcer_integration_tests::integration_test::deposit_withdraw_roundtrip::<PostSetup>(
+                    post_setup,
+                    Init {
+                        bitnames_app: bin_paths.bitnames,
+                        data_dir_suffix: None,
+                    },
+                ).await
+        }.boxed(),
+        file_registry,
+        failure_collector,
+    )
 }
 
 pub fn tests(
     bin_paths: BinPaths,
+    file_registry: TestFileRegistry,
+    failure_collector: TestFailureCollector,
 ) -> Vec<AsyncTrial<BoxFuture<'static, anyhow::Result<()>>>> {
     vec![
-        deposit_withdraw_roundtrip(bin_paths.clone()),
-        ibd_trial(bin_paths.clone()),
-        register_bitname_trial(bin_paths.clone()),
-        unknown_withdrawal_trial(bin_paths),
+        deposit_withdraw_roundtrip(
+            bin_paths.clone(),
+            file_registry.clone(),
+            failure_collector.clone(),
+        ),
+        ibd_trial(
+            bin_paths.clone(),
+            file_registry.clone(),
+            failure_collector.clone(),
+        ),
+        register_bitname_trial(
+            bin_paths.clone(),
+            file_registry.clone(),
+            failure_collector.clone(),
+        ),
+        unknown_withdrawal_trial(bin_paths, file_registry, failure_collector),
     ]
 }

integration_tests/setup.rs
@@ -150,9 +150,13 @@ impl Sidechain for PostSetup {
     ) -> Result<Self, Self::SetupError> {
         let reserved_ports = ReservedPorts::new()?;
         let bitnames_dir = if let Some(suffix) = init.data_dir_suffix {
-            post_setup.out_dir.path().join(format!("bitnames-{suffix}"))
+            post_setup
+                .directories
+                .base_dir
+                .path()
+                .join(format!("bitnames-{suffix}"))
         } else {
-            post_setup.out_dir.path().join("bitnames")
+            post_setup.directories.base_dir.path().join("bitnames")
         };
         std::fs::create_dir(&bitnames_dir)
             .map_err(Self::SetupError::CreateBitNamesDir)?;

LayerTwo-Labs/plain-bitassets

integration_tests/integration_test.rs
@@ -1,6 +1,6 @@
 use bip300301_enforcer_integration_tests::{
     setup::{Mode, Network},
-    util::AsyncTrial,
+    util::{AsyncTrial, TestFailureCollector, TestFileRegistry},
 };
 use futures::{FutureExt, future::BoxFuture};
 
@@ -14,25 +14,53 @@ use crate::{
 
 fn deposit_withdraw_roundtrip(
     bin_paths: BinPaths,
+    file_registry: TestFileRegistry,
+    failure_collector: TestFailureCollector,
 ) -> AsyncTrial<BoxFuture<'static, anyhow::Result<()>>> {
-    AsyncTrial::new("deposit_withdraw_roundtrip", async move {
-        bip300301_enforcer_integration_tests::integration_test::deposit_withdraw_roundtrip::<PostSetup>(
-            bin_paths.others, Network::Regtest, Mode::Mempool,
-            Init {
-                bitassets_app: bin_paths.bitassets,
-                data_dir_suffix: None,
-            },
-        ).await
-    }.boxed())
+    AsyncTrial::new(
+        "deposit_withdraw_roundtrip",
+        async move {
+            let (res_tx, _) = futures::channel::mpsc::unbounded();
+            let post_setup = bip300301_enforcer_integration_tests::setup::setup(
+                &bin_paths.others,
+                Network::Regtest,
+                Mode::Mempool,
+                res_tx
+            ).await?;
+            bip300301_enforcer_integration_tests::integration_test::deposit_withdraw_roundtrip::<PostSetup>(
+                    post_setup,
+                    Init {
+                        bitassets_app: bin_paths.bitassets,
+                        data_dir_suffix: None,
+                    },
+                ).await
+        }.boxed(),
+        file_registry,
+        failure_collector,
+    )
 }
 
 pub fn tests(
     bin_paths: BinPaths,
+    file_registry: TestFileRegistry,
+    failure_collector: TestFailureCollector,
 ) -> Vec<AsyncTrial<BoxFuture<'static, anyhow::Result<()>>>> {
     vec![
-        deposit_withdraw_roundtrip(bin_paths.clone()),
-        ibd_trial(bin_paths.clone()),
-        unknown_withdrawal_trial(bin_paths.clone()),
-        vote_trial(bin_paths),
+        deposit_withdraw_roundtrip(
+            bin_paths.clone(),
+            file_registry.clone(),
+            failure_collector.clone(),
+        ),
+        ibd_trial(
+            bin_paths.clone(),
+            file_registry.clone(),
+            failure_collector.clone(),
+        ),
+        unknown_withdrawal_trial(
+            bin_paths.clone(),
+            file_registry.clone(),
+            failure_collector.clone(),
+        ),
+        vote_trial(bin_paths, file_registry, failure_collector),
     ]
 }

integration_tests/setup.rs
@@ -151,11 +151,12 @@ impl Sidechain for PostSetup {
         let reserved_ports = ReservedPorts::new()?;
         let bitassets_dir = if let Some(suffix) = init.data_dir_suffix {
             post_setup
-                .out_dir
+                .directories
+                .base_dir
                 .path()
                 .join(format!("bitassets-{suffix}"))
         } else {
-            post_setup.out_dir.path().join("bitassets")
+            post_setup.directories.base_dir.path().join("bitassets")
         };
         std::fs::create_dir(&bitassets_dir)
             .map_err(Self::SetupError::CreateBitAssetsDir)?;